966be3b8720b97af46a195112d48507494392158
ibrahim <ibrahim.bekkari@student.vinci.be>
2023-12-11 20:50:51 +0100

    fix

--- src/Components/Pages/UserSpacePage.js
+++ src/Components/Pages/UserSpacePage.js
@@ -2,7 +2,7 @@
 /* eslint-disable no-plusplus */
 // eslint-disable-next-line no-unused-vars
 import Navigate from '../Router/Navigate';
-import { clearPage, quizLinkEventListeners} from '../../utils/render';
+import { clearPage, quizLinkEventListeners } from '../../utils/render';
 import getConnectedUserDetails from '../../utils/auths';
 import trophee from '../../img/badge1.jpg';
 import { readAllQuizzesByUser, deleteOneQuiz } from '../../models/quizzes';
@@ -57,7 +57,9 @@ async function renderUserQuiz() {
   } else {
     allQuizzesByUser.forEach((quiz) => {
       mainListQuiz += `   
-      <a id_quiz="${quiz.quiz_id}" class="quiz_link text-decoration-none">
+      <a id_quiz="${quiz.quiz_id}" data-uri="/quiz?id=${
+        quiz.quiz_id
+      }" class="text-decoration-none">
      <div class="row">
      <div class="card shadow cardMyQuiz">
          <div class="card-body">
@@ -88,7 +90,6 @@ async function renderUserQuiz() {
     </section>`;
 
   main.innerHTML = mainListQuiz;
-
   quizLinkEventListeners();
 
   const linkBadge = document.querySelector('#linkBadge');
@@ -98,8 +99,6 @@ async function renderUserQuiz() {
   });
   attachDeleteEventListeners();
 }
-
-
 
 function attachDeleteEventListeners() {
   const deleteButtons = document.querySelectorAll('.delete-quiz-btn');
